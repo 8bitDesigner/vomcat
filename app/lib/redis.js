f686f6373fd568d4ee2e7085d4c7b9957257c289
Paul Sweeney <paul@8-bitdesign.com>
2015-03-02 21:37:54 -0800

    Connecting to prod redis

--- app/lib/redis.js
+++ app/lib/redis.js
@@ -1,7 +1,18 @@
 var redis = require("redis")
-  , client = redis.createClient()
+  , parser = require("url").parse
+  , client
+
+if (process.env.REDISTOGO_URL) {
+  let url = parser(process.env.REDISTOGO_URL)
+    , pass = url.auth.split(':').pop()
+
+  client = redis.createClient(url.port, url.hostname)
+  client.auth(pass)
+} else {
+  client = redis.createClient();
+}
 
 module.exports = function(db) {
-  client.select(db || 1)
+  client.select(db || process.env.REDISTOGO_DB || 1)
   return client
 }
