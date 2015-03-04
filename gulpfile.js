46bf89aa1f471d5ab99fbda33391cef49e5b598a
Paul Sweeney <paul@8-bitdesign.com>
2015-03-03 22:48:26 -0800

    Adding sass compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp')
   , source = require('vinyl-source-stream')
+  , sass = require('gulp-sass')
   , browserify = require('browserify')
   , babelify = require('babelify')
 
 gulp.task('build:js', function() {
-  browserify('./app/public/jsx/app.jsx')
+  return browserify('./app/public/jsx/app.jsx')
   .require('./app/public/jsx/app.jsx', {expose: 'App'})
   .transform(babelify)
   .bundle()
@@ -12,9 +13,18 @@ gulp.task('build:js', function() {
   .pipe(gulp.dest('./app/public/js'))
 })
 
+gulp.task('build:css', function () {
+  return gulp.src('./app/public/scss/**/*.scss')
+  .pipe(sass({
+    includePaths: ['./node_modules/bootstrap-sass/assets/stylesheets']
+  }))
+  .pipe(gulp.dest('./app/public/css'))
+});
+
 gulp.task('watch', ['default'], function() {
   gulp.watch('./app/public/jsx/**/*.jsx', ['build:js'])
+  gulp.watch('./app/public/scss/**/*.scss', ['build:css'])
 })
 
-gulp.task('default', ['build:js'])
+gulp.task('default', ['build:js', 'build:css'])
 
