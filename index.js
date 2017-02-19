b6e73430e1f3ba13d39429fe46f7e3f34f993213
Paul Sweeney <paul@8-bitdesign.com>
2017-02-18 20:59:52 -0800

    Migrating DB to DynamoDB

--- index.js
+++ index.js
@@ -1,65 +1,68 @@
-require("babel/register");
+require('babel/register')
 
-var express = require('express')
-  , path = require('path')
-  , server = express()
-  , port = process.env.PORT || 3001
-  , redis = require('./lib/redis')()
-  , VomModel = require('./lib/voms')
-  , voms = new VomModel(redis)
-  , App = require('./app')
+if ((process.env.NODE_ENV || 'development') === 'development') {
+  require('dotenv').load()
+}
+
+const express = require('express')
+const path = require('path')
+const server = express()
+const port = process.env.PORT || 3001
+const voms = require('./lib/voms-dynamo.js')
+const App = require('./app')
 
 // view engine setup
-server.set('views', path.join(__dirname, 'app', 'views'));
-server.set('view engine', 'ejs');
+server.set('views', path.join(__dirname, 'app', 'views'))
+server.set('view engine', 'ejs')
 
 // Set up heroku heartbeet
 // require('./lib/keepalive')(server, { route: 'keepalive', timeout: 600000 })
 
 // Serve static assets
-server.use(express.static('app/public' /*, { maxAge: 86400000 } */));
-server.use(express.static('node_modules/bootstrap/dist', { maxAge: 86400000 }));
+server.use(express.static('app/public' /*, { maxAge: 86400000 } */))
+server.use(express.static('node_modules/bootstrap/dist', { maxAge: 86400000 }))
 
 // Serve errything else
-server.get('/', function(req, res) {
-  voms.get(function(err, dates) {
-    app = new App(dates)
+server.get('/', function (req, res, next) {
+  voms.get(function (err, dates) {
+    const app = new App(dates)
 
-    res.render('index', {
-      title: 'Vomcat',
-      markup: app.start(),
-      data: JSON.stringify(dates)
-    })
+    if (err) {
+      next(err)
+    } else {
+      res.render('index', {
+        title: 'Vomcat',
+        markup: app.start(),
+        data: JSON.stringify(dates)
+      })
+    }
   })
-
 })
 
-server.get('/voms', function(req, res) {
-  voms.get(function(err, dates) {
+server.get('/voms', function (req, res, next) {
+  voms.get(function (err, dates) {
     if (err) { return next(err) }
     res.send(dates)
   })
 })
 
-server.put('/voms', function(req, res) {
-  voms.create(function(err) {
+server.put('/voms', function (req, res, next) {
+  voms.create(function (err) {
+    err ? next(err) : res.status(204).send()
+  })
+})
+
+server.delete('/voms/:timestamp', function (req, res, next) {
+  voms.remove(req.params.timestamp, function (err) {
     if (err) { return next(err) }
     res.status(204).send()
   })
 })
 
-server.delete('/voms/:timestamp', function(req, res) {
-  voms.remove(req.params.timestamp, function(err) {
-    if (err) { return next(err) }
-    res.status(204).send()
-  })
-})
-
-server.use(function(err, req, res, next) {
+server.use(function (err, req, res, next) {
   res.render('error', {message: err.message, error: err})
 })
 
-server.listen(port, function() {
+server.listen(port, function () {
   console.log('Now listening on port ', port)
 })
-
